dc3a2440689ccf12327b4277ba3c01d00bc63ae1
M Saqlain <saqlain2204@gmail.com>
2023-10-17 23:05:54 +0530

    made changes to the model

--- Student Academic Performace Analysis/Streamlit/app.py
+++ Student Academic Performace Analysis/Streamlit/app.py
@@ -5,12 +5,12 @@ import pickle
 with open('model.pkl', 'rb') as file:
     model= pickle.load(file)
 st.title("Student Performance Predictor")
-health = st.slider("Select the health metric", 1,5, key='health')
+g1 = st.number_input("Enter the second period grade", step=1, min_value=0, max_value=20, key='g1')
 age = st.number_input("Enter your age", step=1, key='age')
 famrel = st.slider("Select the family relationship", 1,5, key='famrel')
 absences = st.number_input("Enter the number of absences", step=1, key='absences')
 g2 = st.number_input("Enter the second period grade", step=1, min_value=0, max_value=20, key='g2')
 
 if st.button("Predict"):
-    prediction = model.predict([[g2, absences, age, famrel, health]])
+    prediction = model.predict([[g2, absences, age, famrel, g1]])
     st.subheader(f"Your grade 3 is most likely to be {prediction[0]}")
